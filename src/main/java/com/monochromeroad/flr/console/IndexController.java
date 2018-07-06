fc3c835566af5b4f2743f96a8aea9d6cb834f2c1
literalice <literalice@monochromeroad.com>
2018-07-06 14:58:33 +0900

    add beers

--- src/main/java/com/monochromeroad/flr/console/IndexController.java
+++ src/main/java/com/monochromeroad/flr/console/IndexController.java
@@ -19,11 +19,13 @@ public class IndexController {
         descriptions.add("シメイブルー。シメイ・ブルーは、もともと1948年にクリスマスビールとしてリリースされたもので、人気を集め、現在では通年で生産されています。");
         descriptions.add("乾杯という文化");
         descriptions.add("ビール腹。飲み過ぎるとなる");
+        descriptions.add("オルヴァル。ほかのトラピスト醸造所と違い、オルヴァル修道院が造っているビールはたった1種類だけ。敷地内にあるマチルドの泉には１０７６年にここを訪れたイタリアの伯爵夫人が結婚指輪を落とし、鱒がくわえて上がってきたという伝説が残っいる。");
+        descriptions.add("よなよなエール。長野県の地ビールメーカー「株式会社ヤッホーブルーイング」(愛称・よなよなの里)によるビール銘柄の一つ。常温・短期間で発酵させて作るエール・ビールのうち「アメリカン・ペールエール」と呼ばれる種類に属する。");
 
         model.addAttribute("message", "適当にビールの情報を出します!");
 
         Random rndSeed = new Random();
-        int beerIndex = rndSeed.nextInt(3);
+        int beerIndex = rndSeed.nextInt(descriptions.size());
 
         String imageURL = "https://s3-ap-northeast-1.amazonaws.com/beer-console-images/beer-" + Integer.toString(beerIndex) + ".jpg";
         model.addAttribute("imageURL", imageURL);
