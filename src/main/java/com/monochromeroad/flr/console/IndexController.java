ea6bda6a5c029c24d9ed7d3dd908ad0b0048e113
literalice <literalice@monochromeroad.com>
2018-07-06 13:11:27 +0900

    adds description

--- src/main/java/com/monochromeroad/flr/console/IndexController.java
+++ src/main/java/com/monochromeroad/flr/console/IndexController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
 
 @Controller
 public class IndexController {
@@ -12,12 +14,20 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model) {
         String smell = "におうな";
-        model.addAttribute("message", "適当にビールの画像を出します!");
+
+        List<String> descriptions = new ArrayList<>();
+        descriptions.add("シメイブルー。シメイ・ブルーは、もともと1948年にクリスマスビールとしてリリースされたもので、人気を集め、現在では通年で生産されています。");
+        descriptions.add("乾杯という文化");
+        descriptions.add("ビール腹。飲み過ぎるとなる");
+
+        model.addAttribute("message", "適当にビールの情報を出します!");
 
         Random rndSeed = new Random();
-        int imageIndex = rndSeed.nextInt(3);
-        String imageURL = "https://s3-ap-northeast-1.amazonaws.com/beer-console-images/beer-" + Integer.toString(imageIndex) + ".jpg";
+        int beerIndex = rndSeed.nextInt(3);
+
+        String imageURL = "https://s3-ap-northeast-1.amazonaws.com/beer-console-images/beer-" + Integer.toString(beerIndex) + ".jpg";
         model.addAttribute("imageURL", imageURL);
+        model.addAttribute("description", descriptions.get(beerIndex));
 
         return "index";
     }
